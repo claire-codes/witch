7cc0f3dfe326c6171a0c6500499767a98b0c5417
Claire Parker <claire.parker87@gmail.com>
2016-05-05 21:49:22 +0300

    :dolls: Show/hide filter

--- js/whichFilter.js
+++ js/whichFilter.js
@@ -2,17 +2,24 @@ $(function() {
     'use strict';
 
     var $filterBox = document.getElementById('filter-box');
-    $filterBox.value = 'foo';
     var filterTerm;
+    var $listItems = document.querySelectorAll("#repo-list li");
+    var $listToFilter = document.querySelectorAll("#repo-list li .tech");
     $filterBox.addEventListener("keyup", function() {
         if ($filterBox.value.length > 2) {
-            var $listToFilter = document.querySelectorAll("#repo-list li .tech");
             filterTerm = $filterBox.value;
             console.log(filterTerm);
             for (var i = 0; i < $listToFilter.length; i++) {
                 if (($listToFilter[i].innerHTML).match(filterTerm)) {
                     console.log("match");
+                    $listItems[i].style.display = "block";
+                } else {
+                    $listItems[i].style.display = "none";
                 }
+            }
+        } else {
+            for (var i = 0; i < $listToFilter.length; i++) {
+                $listItems[i].style.display = "block";
             }
         }
     })
