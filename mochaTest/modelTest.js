ce3ba684f6354dd12b570d2d36ee62c1b4d94363
Claire Parker <claire.parker87@gmail.com>
2016-05-14 22:12:16 +0100

    :construction: Add Mocha tests

---
+++ mochaTest/modelTest.js
@@ -0,0 +1,43 @@
+"use strict";
+
+describe("WhichModel", function() {
+
+    beforeEach(function() {
+        this.model = new app.whichModel();
+    });
+    
+    var testRepos = {
+    		"clairex": ["module", "javascript"],
+    		"generator-clekyll": ["yeoman", "clairex", "javascript"],
+    		"left-weeks": ["javascript"]
+    	};
+
+    it("should be a Backbone Model", function() {
+        expect(this.model).to.be.an("object");
+        expect(this.model).to.be.an.instanceof(Backbone.Model);
+    });
+
+    it("should have certain defaults", function() {
+        expect(this.model.get("repos")).to.eql({});
+        expect(this.model.get("uniqueTech")).to.eql([]);
+        expect(this.model.url).to.eq("data/repos.json");
+    });
+
+    describe("#getUniqueTech", function() {
+        it("will return blank if repos is blank", function() {
+            this.model.getUniqueTech();
+            expect(this.model.get("uniqueTech")).to.eql([]);
+        });
+
+        it("will flatten them into a list", function() {
+            this.model.set("repos", testRepos);
+            this.model.getUniqueTech();
+            expect(this.model.get("uniqueTech")).to.eql(["module","javascript","yeoman","clairex"]);
+        });
+
+        it("will remove dupes", function() {
+
+        });
+    });
+
+});
