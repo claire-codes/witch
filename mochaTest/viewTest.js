1046d4bd515bd8748746c7da0cf62346e80bd744
Claire Parker <claire.parker87@gmail.com>
2016-05-15 22:16:00 +0100

    :black_medium_square: Add basic view tests

--- mochaTest/viewTest.js
+++ mochaTest/viewTest.js
@@ -1,43 +1,39 @@
 "use strict";
 
-xdescribe("view", function() {
+describe("view", function() {
 
-  beforeEach(function() {
-    $("body").append("<div id='weeks-view-test'></div>");
-    this.view = new app.WeeksView({
-      el: $("#weeks-view-test")
+    beforeEach(function() {
+        $("body").append("<div id='which-view-test'></div>");
+        this.view = new app.whichView({
+            el: $("#which-view-test")
+        });
     });
-  });
 
-  afterEach(function() {
-    this.view = null;
-  });
+    afterEach(function() {
+        this.view = null;
+    });
 
-  it("should be a Backbone View object", function() {
-    expect(this.view).to.be.ok;
-    expect(this.view).to.be.an("object");
-    expect(this.view).to.be.an.instanceof(Backbone.View);
-  });
+    it("should be a Backbone View object", function() {
+        expect(this.view).to.be.ok;
+        expect(this.view).to.be.an("object");
+        expect(this.view).to.be.an.instanceof(Backbone.View);
+    });
 
-  it("should have default div tagname", function() {
-    expect(this.view.tagName).to.equal("div");
-    expect(this.view.el.tagName.toLowerCase()).to.equal("div");
-  });
+    it("should have default div tagname", function() {
+        expect(this.view.tagName).to.equal("div");
+        expect(this.view.el.tagName.toLowerCase()).to.equal("div");
+    });
 
-  it("render static text and variables correctly", function() {
-    this.view.render();
+    it("render static text and variables correctly", function() {
+        // this.view.model.getUniqueTech();
+        this.view.render();
 
-    expect(this.view.$el.html().match(/Left Weeks/)).to.be.ok;
-    expect(this.view.$el.html().match(/You are currently 35/)).to.be.ok;
-  });
+        expect(this.view.$el.html().match(/Which project did I use that in?/)).to.be.ok;
+        // expect(this.view.$el.html().match(/clairex: es6, chaining/)).to.be.ok;
+    });
 
-  it("should have a model associated with it", function() {
-    expect(this.view.model).to.be.an.instanceof(Backbone.Model);
-  });
+    it("should have a model associated with it", function() {
+        expect(this.view.model).to.be.an.instanceof(Backbone.Model);
+    });
 
-  it("will have as many special divs as there are weeksLeft", function() {
-    this.view.model.set("years", 36);
-    expect(this.view.model.leftWeeks()).to.equal(51);
-    expect($(".week-div").length).to.equal(1836);
-  });
 });
